d6b01e896558d7b42f79874f2f668441843e8867
Andile Mbatha <71871756+ANDILE805@users.noreply.github.com>
2025-06-30 13:10:39 +0200

    Update narrator.py

--- narrator.py
+++ narrator.py
@@ -1,4 +1,16 @@
+import streamlit as st
 import boto3
+
+aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
+aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
+region_name = st.secrets["aws"]["region_name"]
+
+polly = boto3.client(
+    "polly",
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=region_name
+)
 
 polly = boto3.client('polly', region_name='us-east-1')
 
