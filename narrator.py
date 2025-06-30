ef12d50b1277857b7ffaec41d430b4676d6e3b8b
Andile Mbatha <71871756+ANDILE805@users.noreply.github.com>
2025-06-30 13:25:33 +0200

    Update narrator.py

--- narrator.py
+++ narrator.py
@@ -1,31 +1,27 @@
 import streamlit as st
 import boto3
 
-aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
-aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
-region_name = st.secrets["aws"]["region_name"]
+def synthesize_text_to_speech(text, voice='Joanna'):
+   
+    aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
+    aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
+    region_name = st.secrets["aws"]["region_name"]
 
-polly = boto3.client(
-    "polly",
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-    region_name=region_name
-)
-
-polly = boto3.client('polly', region_name='us-east-1')
-
-
-text = "Hello! This is your first narrated voice using Amazon Polly."
-
-response = polly.synthesize_speech(
-    Text=text,
-    OutputFormat='mp3',
-    VoiceId='Joanna'  
-)
+ 
+    polly = boto3.client(
+        "polly",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name
+    )
 
 
-with open('output.mp3', 'wb') as file:
-    file.write(response['AudioStream'].read())
+    response = polly.synthesize_speech(
+        Text=text,
+        OutputFormat='mp3',
+        VoiceId=voice
+    )
     
-
-print("Narration saved as output.mp3")
+  
+    audio_bytes = response['AudioStream'].read()
+    return audio_bytes
