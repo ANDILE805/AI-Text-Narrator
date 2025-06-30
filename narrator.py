bf6be258f8aaed7e541ce6d2188a3483be08053b
Andile Mbatha <71871756+ANDILE805@users.noreply.github.com>
2025-06-30 13:33:06 +0200

    Update narrator.py

--- narrator.py
+++ narrator.py
@@ -2,12 +2,10 @@ import streamlit as st
 import boto3
 
 def synthesize_text_to_speech(text, voice='Joanna'):
-   
     aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
     aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
     region_name = st.secrets["aws"]["region_name"]
 
- 
     polly = boto3.client(
         "polly",
         aws_access_key_id=aws_access_key_id,
@@ -15,13 +13,12 @@ def synthesize_text_to_speech(text, voice='Joanna'):
         region_name=region_name
     )
 
-
     response = polly.synthesize_speech(
         Text=text,
         OutputFormat='mp3',
         VoiceId=voice
     )
     
-  
     audio_bytes = response['AudioStream'].read()
     return audio_bytes
+
